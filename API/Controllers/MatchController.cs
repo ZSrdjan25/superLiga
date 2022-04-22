927878ca4d344211d0326333c6d5d1fc9efa6329
Srdjan Zoranovic <srdjan.zoranovic@gmail.com>
2022-04-22 11:33:44 +0200

    Match - Model, Controller

---
+++ API/Controllers/MatchController.cs
@@ -0,0 +1,66 @@
+﻿using API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MatchController : ControllerBase
+    {
+        private ApplicationDbContext applicationDbContext;
+        public MatchController(ApplicationDbContext _applicationDbContext)
+        {
+            applicationDbContext = _applicationDbContext;
+        }
+
+        //GET: api/Match
+        [HttpGet]
+        public async Task<List<Match>> GET()
+        {
+            return await applicationDbContext.Match.ToListAsync();
+        }
+
+        //POST: api/Match
+        [HttpPost]
+        public async Task<ActionResult<Match>> POST(Match match)
+        {
+            await applicationDbContext.Match.AddAsync(match);
+            await applicationDbContext.SaveChangesAsync();
+            return match;
+        }
+
+        //PUT: api/Match
+        [HttpPut]
+        public async Task<ActionResult<Match>> PUT(Match match)
+        {
+            if(match.MatchId != 0)
+            {
+                Match match1 = await applicationDbContext.Match.SingleOrDefaultAsync(x => x.MatchId == match.MatchId);
+                match1.NumberRounds = match.NumberRounds;
+                match1.HomeTeam = match.HomeTeam;
+                match1.GuestTeam = match.GuestTeam;
+                match1.HomeGoals = match.HomeGoals;
+                match1.GuestGoals = match.GuestGoals;
+
+                await applicationDbContext.SaveChangesAsync();
+                return match1;
+            }
+            return new Match();
+        }
+
+        //DELETE: api/Match/{id}
+        [HttpDelete("{id}")]
+        public async Task DELETE(int id)
+        {
+            Match match = await applicationDbContext.Match.SingleOrDefaultAsync(x => x.MatchId == id);
+            applicationDbContext.Match.Remove(match);
+            await applicationDbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ API/Migrations/20220421110211_Second.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Migrations
+{
+    public partial class Second : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Match",
+                columns: table => new
+                {
+                    MatchId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    NumberRounds = table.Column<int>(type: "int", nullable: false),
+                    HomeTeam = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    GuestTeam = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    HomeGoals = table.Column<int>(type: "int", nullable: false),
+                    GuestGoals = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Match", x => x.MatchId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Match");
+        }
+    }
+}
